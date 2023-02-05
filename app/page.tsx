b98d461d13e20086711b53cf203e3c6778b0a975
Luis Matute <luis.matute@me.com>
2023-02-05 22:13:53 +0100

    Fix types

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { redirect } from "next/navigation";
+import { Router } from "next/router";
+import { ReactNode } from "react";
 
-export default function Page() {
-  redirect('/chat');
+export default function Page(): ReactNode | Promise<ReactNode> {
+  return redirect('/chat');
 }
